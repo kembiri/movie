f9b75a46b3b35c61925626770f0953f25f70368b
kembiri <kembiri1@gmail.com>
2025-07-09 04:25:38 -0700

    Create movie.js

---
+++ movie.js
@@ -0,0 +1,47 @@
+const movieData = [
+  {
+    title: "The Flu (2013)",
+    slug: "the-flu-2013",
+    type: "Movie",
+    genre: ["Romantis", "Komedi"],
+    country: "Korea",
+    poster:
+      "https://blogger.googleusercontent.com/img/a/AVvXsEjsqZle7bjW-OIXVZ3QOjz8UbqVjoAU_HkiGcK_sITlKIH_I_SVnm82lNrg5yld8vhuBYWbj7hcioGMhx3ffWKAxvNzeqLi9ZhK_HJ2FF16FFTJvZtYscASwTXUdJOllFY_XDSI84LbANfhxpepcnfXOJvY4wL-EYRlSU0T3RewQWDvrqCN7RgqDJN1",
+    image:
+      "https://dkfhw9rzsr80z.cloudfront.net/TVN-ID-001-A1035/artworks/posters/TVN-ID-001-A1035-P704.jpg?hash=1604367408000",
+    url: "https://firebasestorage.googleapis.com/v0/b/bangden-com.appspot.com/o/upload%2Fflu%202013%20mp4.mp4?alt=media&token=",
+  },
+  {
+    title: "Kung Fu Hustle (2004)",
+    slug: "kung-fu-hustle-2004",
+    type: "Movie",
+    genre: ["Action", "Komedi"],
+    country: "China",
+    poster: "https://image.tmdb.org/t/p/w500/exbyTbrvRUDKN2mcNEuVor4VFQW.jpg",
+    image:
+      "https://thumbor.prod.vidiocdn.com/8QSJxkDzeTsey3esZoaloy3VxWE=/785x440/filters:quality(70)/vidio-web-prod-video/uploads/video/image/7448477/kung-fu-hustle-d7a214.jpg",
+    url: "https://firebasestorage.googleapis.com/v0/b/bangden-com.appspot.com/o/upload%2FD21%20FUN%20Kung%20fu%20hustle%202004%20mp4.mp4?alt=media&token=",
+  },
+  {
+    title: "Shaolin Soccer (2001)",
+    slug: "shaolin-soccer-2001",
+    type: "Movie",
+    genre: ["Action", "Komedi"],
+    country: "China",
+    poster: "https://image.tmdb.org/t/p/w500/kVpq68NxChzynK7otaymRSaWR8p.jpg",
+    image:
+      "https://about.vidio.com/wp-content/uploads/2022/07/SVOD-Shaolin-Soccer-Alt-A-Thumbnail.jpg",
+    url: "https://firebasestorage.googleapis.com/v0/b/bangden-com.appspot.com/o/upload%2FD21%20FUN%20Shaolin%20soccer%202001%20mp4.mp4?alt=media&token=",
+  },
+  {
+    title: "Kingdom of Heaven (2005)",
+    slug: "kingdom-of-heaven-2005",
+    type: "Movie",
+    genre: ["Action", "Adventure", "Drama"],
+    country: "Maroko",
+    poster: "https://image.tmdb.org/t/p/w500/rNaBe4TwbMef71sgscqabpGKsxh.jpg",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMzdhNGZhMjQtYjVmNy00OWMyLTk1MmQtZGNhZDYwMzZlZjI3XkEyXkFqcGc@._V1_SX300.jpg",
+    url: "https://firebasestorage.googleapis.com/v0/b/bangden-com.appspot.com/o/upload%2FD21.FUN-Kingdom%20of%20Heaven%20(2005)-1080p.mp4?alt=media&token=e8d1eb06-0583-4892-8122-36c2b956201e",
+  },
+];
